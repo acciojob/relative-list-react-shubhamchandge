76ec030f402c2f175e21f295b8d09bca258f48c0
shubhamchandge <shubhamchandge@gmail.comm>
2023-02-24 11:23:10 +0530

    Solution

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import React from 'react'
 const App = () => {
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <ol key={relativeList}>
+        <li key={relativeListItem1} > goa</li>
+        <li key={relativeListItem2}> kerla </li>
+      </ol>
     </div>
   )
 }
